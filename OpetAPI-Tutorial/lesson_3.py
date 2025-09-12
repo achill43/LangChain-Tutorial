619ccf380ed3c5a3c2fe1471dbd8945857323718
Sergei Sorochinskii <sergei.sorochinskii93@gmail.com>
2025-09-12 11:45:35 +0300

    Added OpenAI API tutorials part 1

---
+++ OpetAPI-Tutorial/lesson_3.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response = client.responses.create(
+    model="gpt-5",
+    reasoning={"effort": "low"},
+    instructions="Talk like a pirate.",
+    input=[
+        {
+            "role": "developer",
+            "content": "Talk like a pirate."
+        },
+        {
+            "role": "user",
+            "content": "Are semicolons optional in JavaScript?"
+        }
+    ]
+)
+
+print(response.output_text)
